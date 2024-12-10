7b0d2a7f5790a569c0f83fd4d926bcf5d1ceb524
badr274 <145036775+badr274@users.noreply.github.com>
2024-12-10 19:31:07 +0200

    finishing the slider in home page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//Start  Handle showing nav menu at small screens
+//Start  Handle Function addAndRemoveClassShow
 const overlay = document.querySelector(".overlay");
 const menuIcon = document.querySelector("nav .menu-icon");
 const navMenu = document.querySelector("nav .menu");
@@ -31,7 +31,7 @@ controlAddandRemoveClassShow(
   shoppingCart,
   closeShoppingCartIcon
 );
-//End  Handle showing nav menu at small screens
+//End  Handle Function addAndRemoveClassShow
 // =========================================
 // =========================================
 // =========================================
@@ -62,7 +62,6 @@ allSmallLinksHeaders.forEach((header) => {
 // =========================================
 // =========================================
 // Start Handle showing advies content in checkout page
-// const advices = document.querySelectorAll(".checkout-page .advices");
 const advices = document.querySelectorAll(".checkout-page .advices .advice");
 const advicesName = document.querySelectorAll(
   ".checkout-page .advices .advice .advice-name"
@@ -81,3 +80,133 @@ advicesName.forEach((adviceName) => {
   });
 });
 // End Handle showing advies content in checkout page
+// ========================================
+// ========================================
+// ========================================
+// Start Handling My Slider
+// ===================================
+// First define images that will be displayed
+// ===================================
+const sliderImages = [
+  "../images/slider-image 1.png",
+  "../images/slider-image 2.png",
+  "../images/slider-image 2.png",
+  "../images/slider-image 1.png",
+];
+const mySlider = document.querySelector(".my-slider");
+const sliderSContainer = document.querySelector(
+  ".my-slider .sliders-container"
+);
+const dots = document.querySelector(".my-slider .dots");
+let currentSlide = 0;
+let numberOfSlide = 1;
+// ===================================
+// Second make slider elements and classes
+// ===================================
+for (let image of sliderImages) {
+  let slide = document.createElement("div");
+  let slideImage = document.createElement("img");
+  let slideContent = document.createElement("div");
+  let slideDesc = document.createElement("div");
+  let slideNumber = document.createElement("div");
+  let theNumber = document.createElement("div");
+  let space = document.createElement("div");
+  let bedroom = document.createElement("div");
+  let slideType = document.createElement("div");
+  let nextArrow = document.createElement("div");
+  let dot = document.createElement("div");
+  let innerDot = document.createElement("span");
+  dot.id = `slide-${numberOfSlide}`;
+  dot.className = "dot";
+  dot.appendChild(innerDot);
+  dots.appendChild(dot);
+  slide.className = "slide";
+  slide.dataset.target = `slide-${numberOfSlide}`;
+  slide.style.order = `${numberOfSlide}`;
+  slideImage.src = image;
+  slideImage.className = "slide-image";
+  slideContent.className = "slide-content";
+  slideDesc.className = "slide-desc";
+  slideNumber.className = "slide-number";
+  theNumber.className = "the-number";
+  space.className = "space";
+  slideType.className = "slide-type";
+  nextArrow.className = "next-arrow";
+  slideNumber.innerHTML = `0${numberOfSlide}`;
+  bedroom.innerHTML = "Bed Room";
+  slideType.innerHTML = "Inner Peace";
+  slideNumber.append(theNumber, space, bedroom);
+  slideDesc.append(slideNumber, slideType);
+  slideContent.append(slideDesc, nextArrow);
+  slide.append(slideImage, slideContent);
+  sliderSContainer.append(slide);
+  numberOfSlide++;
+}
+// ===================================
+// Handle Dots Pagination
+// ===================================
+const slides = document.querySelectorAll(".my-slider .slide");
+const sliderDots = document.querySelectorAll(".my-slider .dots .dot");
+// ===================================
+// Put class active on first slide by currentSlideVariable
+// ===================================
+function getActiveSlide(currentSlide) {
+  slides[currentSlide].classList.add("active");
+}
+getActiveSlide(currentSlide);
+sliderDots.forEach((dot) => {
+  // put class active on current dot
+  slides.forEach((slide) => {
+    if (slide.dataset.target === dot.id && slide.classList.contains("active")) {
+      dot.classList.add("active");
+    }
+  });
+
+  dot.addEventListener("click", () => {
+    // Link between current dot and slide
+    slides.forEach((slide, i) => {
+      if (slide.dataset.target === dot.id) {
+        slides.forEach((slide) => {
+          slide.classList.remove("active");
+        });
+        slide.classList.add("active");
+        currentSlide = i;
+      }
+    });
+    sliderDots.forEach((dot) => {
+      dot.classList.remove("active");
+    });
+    dot.classList.add("active");
+  });
+});
+// ==============================
+// Handle Next Arrow Paginater
+// ==============================
+const goNext = document.querySelector(".my-slider .right-arrow");
+goNext.addEventListener("click", () => {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  getActiveSlide(currentSlide);
+  sliderDots.forEach((dot) => {
+    dot.classList.remove("active");
+    slides.forEach((slide) => {
+      if (
+        slide.dataset.target === dot.id &&
+        slide.classList.contains("active")
+      ) {
+        dot.classList.add("active");
+      }
+    });
+  });
+});
+// End Handling My Slider
+// ================================
+// ================================
+// ================================
+// ================================
