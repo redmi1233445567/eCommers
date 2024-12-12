729942c4357cb698c0f946a1db6e08b6ff26a1b3
badr274 <145036775+badr274@users.noreply.github.com>
2024-12-12 21:39:59 +0200

    finish of my work

--- src/main.js
+++ src/main.js
@@ -210,3 +210,169 @@ goNext.addEventListener("click", () => {
 // ================================
 // ================================
 // ================================
+// Start Logic of Our Products Section
+const products = [
+  {
+    id: 1,
+    proImg: "./img/image 1.png",
+    proName: "Product name1",
+    proDiscription: "Sort Description1",
+    proPrice: "3.500.000",
+    newPrice: "2.500.000",
+    proDiscount: 30,
+    new: false,
+    like: false,
+  },
+  {
+    id: 2,
+    proImg: "./img/image 1.png",
+    proName: "Product name2",
+    proDiscription: "Sort Description2",
+    proPrice: "2.500.000",
+    newPrice: "",
+    new: false,
+    like: false,
+  },
+  {
+    id: 3,
+    proImg: "./img/image 1.png",
+    proName: "Product name3",
+    proDiscription: "Sort Description3",
+    proPrice: "7.000.000",
+    newPrice: "3.500.000",
+    proDiscount: 50,
+    new: false,
+    like: false,
+  },
+  {
+    id: 4,
+    proImg: "./img/image 1.png",
+    proName: "Product name4",
+    proDiscription: "Sort Description4",
+    proPrice: "500.000",
+    newPrice: "",
+    new: true,
+    like: false,
+  },
+  {
+    id: 5,
+    proImg: "./img/image 1.png",
+    proName: "Product name5",
+    proDiscription: "Sort Description5",
+    proPrice: "1.500.000",
+    newPrice: "",
+    like: false,
+  },
+  {
+    id: 6,
+    proImg: "./img/image 1.png",
+    proName: "Product name6",
+    proDiscription: "Sort Description6",
+    proPrice: "150.000",
+    newPrice: "",
+    new: true,
+    like: false,
+  },
+  {
+    id: 7,
+    proImg: "./img/image 1.png",
+    proName: "Product name7",
+    proDiscription: "Sort Description7",
+    proPrice: "7.000.000",
+    newPrice: "3.500.000",
+    proDiscount: 50,
+    like: false,
+  },
+  {
+    id: 8,
+    proImg: "./img/image 1.png",
+    proName: "Product name8",
+    proDiscription: "Sort Description8",
+    proPrice: "500.000",
+    newPrice: "",
+    new: true,
+    like: false,
+  },
+];
+const productsContainer = document.querySelector(
+  ".our-products-section .the-products"
+);
+products.forEach((product) => {
+  let card = document.createElement("div");
+  card.className = "card";
+  let cardImage = document.createElement("div");
+  cardImage.className = "card-image";
+  let image = document.createElement("img");
+  image.src = product.proImg;
+  let theDiscount = document.createElement("span");
+  let newProduct = document.createElement("span");
+  let cardInfo = document.createElement("div");
+  cardInfo.className = "card-info";
+  let cardTitle = document.createElement("h4");
+  cardTitle.className = "card-title";
+  cardTitle.innerHTML = product.proName;
+  let cardDesc = document.createElement("p");
+  cardDesc.className = "card-desc";
+  cardDesc.innerHTML = product.proDiscription;
+  let thePrice = document.createElement("div");
+  thePrice.className = "the-price";
+  let price = document.createElement("span");
+  let oldPrice = document.createElement("del");
+  price.className = "price";
+  price.innerHTML = product.proPrice;
+  if (product.proDiscount) {
+    theDiscount.className = "the-discount";
+    theDiscount.innerHTML = `${product.proDiscount}%`;
+    price.innerHTML = product.newPrice;
+    oldPrice.className = "old-price";
+    oldPrice.innerHTML = product.proPrice;
+  }
+  if (product.new) {
+    newProduct.className = "new-product";
+    newProduct.innerHTML = `New`;
+  }
+  let addCardBtnAndActions = document.createElement("div");
+  addCardBtnAndActions.className = "add-card-and-actions";
+  addCardBtnAndActions.innerHTML = `
+  <button type="button" class="add-to-card-btn my-btn my-light-btn">Add To Card</button>
+  <div class="actions">
+    <div class="share">
+      <i class="fa-solid fa-share-nodes"></i>
+      <span>Share</span>
+    </div>
+    <div class="compare">
+      <i class="fa-solid fa-right-left"></i>
+      <span>Compare</span>
+    </div>
+    <div class="like" id="like-${product.id}">
+      <svg width="16" height="16" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M8.16668 3.5C4.94551 3.5 2.33334 6.08533 2.33334 9.275C2.33334 11.8498 3.35418 17.9608 13.4027
+         24.1383C13.5827 24.2479 13.7893 24.3058 14 24.3058C14.2107 24.3058 14.4173 24.2479 14.5973
+          24.1383C24.6458 17.9608 25.6667 11.8498 25.6667 9.275C25.6667 6.08533 23.0545 3.5 19.8333
+           3.5C16.6122 3.5 14 7 14 7C14 7 11.3878 3.5 8.16668 3.5Z"
+            stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      <span>Like</span>
+    </div>
+  </div>
+  `;
+  cardImage.append(image, theDiscount, newProduct);
+  thePrice.append(price, oldPrice);
+  cardInfo.append(cardTitle, cardDesc, thePrice);
+  card.append(cardImage, cardInfo, addCardBtnAndActions);
+  productsContainer.appendChild(card);
+});
+// End Logic of Our Products Sectio
+const likes = document.querySelectorAll(
+  ".our-products-section .card .actions .like"
+);
+
+likes.forEach((like) => {
+  like.addEventListener("click", () => {
+    const heartLike = document.querySelector(
+      `.our-products-section .card .actions #${like.id} svg`
+    );
+    heartLike.classList.toggle("active");
+    console.log(heartLike);
+  });
+});
